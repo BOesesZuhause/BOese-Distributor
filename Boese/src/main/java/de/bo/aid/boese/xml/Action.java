b639858f2627302e69b185cf9364c2803cf07ddb
Robin <Robinpeuker@web.de>
2015-09-04 12:19:17 +0200

    Modified xml parser.
    Now actions can activate, deactivate and delete todos
    
    Change-Id: I0c72e7bed28ccbda16a8e27f5e515f24b1693c3e

--- Boese/src/main/java/de/bo/aid/boese/xml/Action.java
+++ Boese/src/main/java/de/bo/aid/boese/xml/Action.java
@@ -10,6 +10,9 @@ import org.w3c.dom.NodeList;
 public class Action extends BoeseXML {
 	private HashSet<Integer> activateRuleList;
 	private HashSet<Integer> deactivateRuleList;
+	private HashSet<Integer> deactivateTodoList;
+	private HashSet<Integer> activateTodoList;
+	private HashSet<Integer> deleteTodoList;
 	private HashSet<Component> actorList;
 	
 	public Action(XMLType type, Document doc) {
@@ -17,6 +20,9 @@ public class Action extends BoeseXML {
 		this.doc = doc;
 		activateRuleList = new HashSet<>();
 		deactivateRuleList = new HashSet<>();
+		activateTodoList = new HashSet<>();
+		deactivateTodoList = new HashSet<>();
+		deleteTodoList = new HashSet<>();
 		actorList = new HashSet<>();
 		NodeList nList = doc.getElementsByTagName("ACTIVATE_RULE_ID");
 		for (int i = 0; i < nList.getLength(); i++) {
@@ -34,6 +40,30 @@ public class Action extends BoeseXML {
 				deactivateRuleList.add(Integer.valueOf(element.getTextContent()));
 			}
 		}
+		nList = doc.getElementsByTagName("DEACTIVATE_TODO_ID");
+		for (int i = 0; i < nList.getLength(); i++) {
+			Node node = nList.item(i);
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				Element element = (Element) node;
+				deactivateTodoList.add(Integer.valueOf(element.getTextContent()));
+			}
+		}
+		nList = doc.getElementsByTagName("ACTIVATE_TODO_ID");
+		for (int i = 0; i < nList.getLength(); i++) {
+			Node node = nList.item(i);
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				Element element = (Element) node;
+				activateTodoList.add(Integer.valueOf(element.getTextContent()));
+			}
+		}
+		nList = doc.getElementsByTagName("DELETE_TODO_ID");
+		for (int i = 0; i < nList.getLength(); i++) {
+			Node node = nList.item(i);
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				Element element = (Element) node;
+				deleteTodoList.add(Integer.valueOf(element.getTextContent()));
+			}
+		}
 		nList = doc.getElementsByTagName("ACTOR");
 		for (int i = 0; i < nList.getLength(); i++) {
 			Node node = nList.item(i);
@@ -44,6 +74,7 @@ public class Action extends BoeseXML {
 				double actorResetValue = -1;
 				long actorStartTime = -1;
 				long actorDuration = -1;
+				int actorRepeatAfterEnd = 0;
 				
 				for (int j = 0; j < nActorList.getLength(); j++) {
 					Node nActor = nActorList.item(j);
@@ -63,11 +94,14 @@ public class Action extends BoeseXML {
 					case "DURATION":
 						actorDuration = new Long(nActor.getTextContent()).longValue();
 						break;
+					case "REPEAT_AFTER_END":
+						actorRepeatAfterEnd = new Integer(nActor.getTextContent()).intValue();
+						break;
 					default:
 						break;
 					}
 				}
-				actorList.add(new Component(actorID, actorValue, actorResetValue, actorStartTime, actorDuration));
+				actorList.add(new Component(actorID, actorValue, actorResetValue, actorStartTime, actorDuration, actorRepeatAfterEnd));
 			}
 		}
 	}
@@ -84,5 +118,15 @@ public class Action extends BoeseXML {
 		return actorList;
 	}
 	
+	public HashSet<Integer> getActivateTodos() {
+		return activateTodoList;
+	}
 	
+	public HashSet<Integer> getDeactivateTodos() {
+		return deactivateTodoList;
+	}
+	
+	public HashSet<Integer> getDeleteTodos() {
+		return deleteTodoList;
+	}
 }
--- Boese/src/main/java/de/bo/aid/boese/xml/Component.java
+++ Boese/src/main/java/de/bo/aid/boese/xml/Component.java
@@ -8,6 +8,7 @@ public class Component {
 	protected long startTime;
 	protected long duration;
 	protected Comperator comperator;
+	protected int repeatAfterEnd;
 	
 	public enum Comperator {
 		LOWERTHEN, GREATERTHEN, LOWEREQUAL, GREATEREQUAL, EQUAL, NOTEQUAL
@@ -20,15 +21,21 @@ public class Component {
 		this.startTime = startTime;
 		this.duration = duration;
 		this.comperator = comperator;
+		repeatAfterEnd = 0;
 	}
 	
-	public Component(int id, double value, double resetValue,long startTime, long duration) {
+	public Component(int id, double value, double resetValue,long startTime, long duration, int repeatAfterEnd) {
 		this.id = id;
 		this.value = value;
 		this.resetValue = resetValue;
 		this.startTime = startTime;
 		this.duration = duration;
+		this.repeatAfterEnd = repeatAfterEnd;
 		this.comperator = null;
+	}
+	
+	public int getReatAfterEnd() {
+		return repeatAfterEnd;
 	}
 	
 	public Comperator getComperator() {
