9d909243dfbc583c60f055399853a94c4f51f797
Fabio Spiekermann <fabio.spiekermann@hs-bochum.de>
2015-09-25 13:52:51 +0200

    All Selects
    Zone, Unit, Component, Device, Service, Connector, Rule, Group, Zone,
    User
    
    Change-Id: I8069b3c74279d02696e5af38b029af962b54f5a6

---
+++ Boese/src/main/java/de/bo/aid/boese/db/AllSelects.java
@@ -0,0 +1,208 @@
+package de.bo.aid.boese.db;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.ObjectNotFoundException;
+import org.hibernate.Session;
+
+import de.bo.aid.boese.constants.Errors;
+import de.bo.aid.boese.model.*;
+
+public class AllSelects {
+	
+	/** The connection. */
+	private static Connection connection = Connection.getConnection();
+
+	/**
+	 * All Units
+	 *
+	 * @return the list
+	 */
+	public static List<Unit> Units(){
+		Session session = connection.getSession();
+		session.beginTransaction();
+ 
+		List erg = session.createQuery("from Unit").list();
+		List<Unit> unit = new ArrayList<Unit>();
+		for(Object o: erg){
+			unit.add((Unit) o);
+		}
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		return unit;
+	}
+	
+	/**
+	 * All Components
+	 *
+	 * @return the list
+	 */
+	public static List<Component> Components(){
+		Session session = connection.getSession();
+		session.beginTransaction();
+ 
+		List erg = session.createQuery("from Component").list();
+		List<Component> comp = new ArrayList<Component>();
+		for(Object o: erg){
+			comp.add((Component) o);
+		}
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		return comp;
+	}
+	
+	/**
+	 * All devices
+	 *
+	 * @return the list
+	 */
+	public static List<Device> Devices(){
+		Session session = connection.getSession();
+		session.beginTransaction();
+ 
+		List erg = session.createQuery("from Device").list();
+		List<Device> dev = new ArrayList<Device>();
+		for(Object o: erg){
+			dev.add((Device) o);
+		}
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		return dev;
+	}
+	
+	/**
+	 * All services
+	 *
+	 * @return the list
+	 */
+	public static List<Service> Services(){
+		Session session = connection.getSession();
+		session.beginTransaction();
+ 
+		List erg = session.createQuery("from Service").list();
+		List<Service> serv = new ArrayList<Service>();
+		for(Object o: erg){
+			serv.add((Service) o);
+		}
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		return serv;
+	}
+	
+	/**
+	 * All connectors
+	 *
+	 * @return the list
+	 */
+	public static List<Connector> Connector(){
+		Session session = connection.getSession();
+		session.beginTransaction();
+ 
+		List erg = session.createQuery("from Connector").list();
+		List<Connector> con = new ArrayList<Connector>();
+		for(Object o: erg){
+			con.add((Connector) o);
+		}
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		return con;
+	}
+	
+	/**
+	 * All rules
+	 *
+	 * @return the list
+	 */
+	public static List<Rule> Rules(){
+		Session session = connection.getSession();
+		session.beginTransaction();
+ 
+		List erg = session.createQuery("from Rule").list();
+		List<Rule> rule = new ArrayList<Rule>();
+		for(Object o: erg){
+			rule.add((Rule) o);
+		}
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		return rule;
+	}
+	
+	/**
+	 * All groups
+	 *
+	 * @return the list
+	 */
+	public static List<Group> Groups(){
+		Session session = connection.getSession();
+		session.beginTransaction();
+ 
+		List erg = session.createQuery("from Group").list();
+		List<Group> grp = new ArrayList<Group>();
+		for(Object o: erg){
+			grp.add((Group) o);
+		}
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		return grp;
+	}
+	
+	/**
+	 * All zones
+	 *
+	 * @return the list
+	 */
+	public static List<Zone> Zones(){
+		Session session = connection.getSession();
+		session.beginTransaction();
+ 
+		List erg = session.createQuery("from Zone").list();
+		List<Zone> zone = new ArrayList<Zone>();
+		for(Object o: erg){
+			zone.add((Zone) o);
+		}
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		return zone;
+	}
+	
+	/**
+	 * All users
+	 *
+	 * @return the list
+	 */
+	public static List<User> Users(){
+		Session session = connection.getSession();
+		session.beginTransaction();
+ 
+		List erg = session.createQuery("from User").list();
+		List<User> user = new ArrayList<User>();
+		for(Object o: erg){
+			user.add((User) o);
+		}
+		
+		session.getTransaction().commit();
+		session.close();
+		
+		return user;
+	}
+}
