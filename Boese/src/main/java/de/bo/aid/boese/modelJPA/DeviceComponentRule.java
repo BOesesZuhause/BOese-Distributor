127c348e937dc7ac67206b675ba3ccc185ec9f16
Sebastian Lechte <sebastian.lechte@hs-bochum.de>
2016-05-08 12:40:06 +0200

    Added annotated Entities
    Change-Id: I56adde97f1b43ed0666826a297f203e824a7ee98

---
+++ Boese/src/main/java/de/bo/aid/boese/modelJPA/DeviceComponentRule.java
@@ -0,0 +1,156 @@
+/*             
+ * 			  (                       
+ *			 ( )\         (        (   
+ *			 )((_)  (    ))\ (    ))\  
+ *			((_)_   )\  /((_))\  /((_) 
+ *			 | _ ) ((_)(_)) ((_)(_))   
+ *			 | _ \/ _ \/ -_)(_-</ -_)  
+ *			 |___/\___/\___|/__/\___|
+ *       
+ *           			;            
+ *		      +        ;;;         + 
+ *			  +       ;;;;;        + 
+ *			  +      ;;;;;;;       + 
+ *			  ++    ;;;;;;;;;     ++ 
+ *			  +++++;;;;;;;;;;;+++++  
+ *			   ++++;;;;;;;;;;;+++++  
+ *				++;;;;;;;;;;;;;++    
+ *			     ;;;;;;;;;;;;;;;     
+ *			    ;;;;;;;;;;;;;;;;;     
+ *				:::::::::::::::::    
+ * 				:::::::::::::::::      
+ *  			:::::::::::::::::    
+ *   			::::::@@@@@::::::    
+ *				:::::@:::::@:::::    
+ *				::::@:::::::@::::    
+ * 				:::::::::::::::::    
+ *  			:::::::::::::::::      
+ */
+
+
+
+package de.bo.aid.boese.modelJPA;
+
+// TODO: Auto-generated Javadoc
+/**
+ * DeviceComponentRule Model for Hibernate.
+ */
+public class DeviceComponentRule {
+
+	/** The DeviceComponentRule id. */
+	private DeviceComponentRuleId id;
+	
+	/** The linked devicecomponent. */
+	private DeviceComponent devicecomponent;
+	
+	/** The linked rule. */
+	private Rule rule;
+
+	/**
+	 * Instantiates a new device component rule.
+	 */
+	public DeviceComponentRule() {
+	}
+
+	/**
+	 * Instantiates a new device component rule.
+	 *
+	 * @param id the DeviceComponentRule id
+	 * @param devicecomponent the linked devicecomponent
+	 * @param rule the linked rule
+	 */
+	public DeviceComponentRule(DeviceComponentRuleId id, DeviceComponent devicecomponent, Rule rule) {
+		this.id = id;
+		this.devicecomponent = devicecomponent;
+		this.rule = rule;
+	}
+
+	/**
+	 * Gets the DeviceComponentRule id.
+	 *
+	 * @return the DeviceComponentRule id
+	 */
+	public DeviceComponentRuleId getId() {
+		return this.id;
+	}
+
+	/**
+	 * Sets the DeviceComponentRule id.
+	 *
+	 * @param id the new DeviceComponentRule id
+	 */
+	public void setId(DeviceComponentRuleId id) {
+		this.id = id;
+	}
+
+	/**
+	 * Gets the linked devicecomponent.
+	 *
+	 * @return the linked devicecomponent
+	 */
+	public DeviceComponent getDevicecomponent() {
+		return this.devicecomponent;
+	}
+
+	/**
+	 * Sets the linked devicecomponent.
+	 *
+	 * @param devicecomponent the new linked devicecomponent
+	 */
+	public void setDevicecomponent(DeviceComponent devicecomponent) {
+		this.devicecomponent = devicecomponent;
+	}
+
+	/**
+	 * Gets the linked rule.
+	 *
+	 * @return the linked rule
+	 */
+	public Rule getRule() {
+		return this.rule;
+	}
+
+	/**
+	 * Sets the linked rule.
+	 *
+	 * @param rule the new linked rule
+	 */
+	public void setRule(Rule rule) {
+		this.rule = rule;
+	}
+
+	/**
+	 *  
+	 * To compare two DeviceComponentRules.
+	 *
+	 * @param obj the DeviceComponentRule object to compare
+	 * @return true if both DeviceComponentRules are equal
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DeviceComponentRule other = (DeviceComponentRule) obj;
+		if (devicecomponent == null) {
+			if (other.devicecomponent != null)
+				return false;
+		} else if (!devicecomponent.equals(other.devicecomponent))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (rule == null) {
+			if (other.rule != null)
+				return false;
+		} else if (!rule.equals(other.rule))
+			return false;
+		return true;
+	}
+
+}
---
+++ Boese/src/main/java/de/bo/aid/boese/modelJPA/ToDo.java
@@ -0,0 +1,215 @@
+/*             
+ * 			  (                       
+ *			 ( )\         (        (   
+ *			 )((_)  (    ))\ (    ))\  
+ *			((_)_   )\  /((_))\  /((_) 
+ *			 | _ ) ((_)(_)) ((_)(_))   
+ *			 | _ \/ _ \/ -_)(_-</ -_)  
+ *			 |___/\___/\___|/__/\___|
+ *       
+ *           			;            
+ *		      +        ;;;         + 
+ *			  +       ;;;;;        + 
+ *			  +      ;;;;;;;       + 
+ *			  ++    ;;;;;;;;;     ++ 
+ *			  +++++;;;;;;;;;;;+++++  
+ *			   ++++;;;;;;;;;;;+++++  
+ *				++;;;;;;;;;;;;;++    
+ *			     ;;;;;;;;;;;;;;;     
+ *			    ;;;;;;;;;;;;;;;;;     
+ *				:::::::::::::::::    
+ * 				:::::::::::::::::      
+ *  			:::::::::::::::::    
+ *   			::::::@@@@@::::::    
+ *				:::::@:::::@:::::    
+ *				::::@:::::::@::::    
+ * 				:::::::::::::::::    
+ *  			:::::::::::::::::      
+ */
+
+
+package de.bo.aid.boese.modelJPA;
+
+import java.util.Date;
+
+import de.bo.aid.boese.db.Selects;
+import de.bo.aid.boese.exceptions.DBObjectNotFoundException;
+
+// TODO: Auto-generated Javadoc
+/**
+ * ToDo Model for Hibernate.
+ */
+public class ToDo {
+	
+	/** The ToDoid. */
+	private int toDoId;
+	
+	/**  The time when it will be executed. */
+	private Date date;
+	
+	/**  Is it active. */
+	private boolean active;
+	
+	/**  The Repeat Rule this ToDo is belonging to. */
+	private RepeatRule repeatRule;
+
+	/**
+	 * Instantiates a new to do.
+	 */
+	public ToDo() {
+		
+	}
+
+	/**
+	 * Instantiates a new todo for DB Insert.
+	 *
+	 * @param date the date
+	 */
+	public ToDo(Date date){
+		this.date = date;
+		this.active = true;
+	}
+
+	/**
+	 * Instantiates a new to do with all parameters.
+	 *
+	 * @param toDoId the to do id
+	 * @param date the date
+	 * @param active the active
+	 */
+	public ToDo(int toDoId, Date date, boolean active){
+		this.toDoId = toDoId;
+		this.date = date;
+		this.active = active;
+		//TODO
+		/*
+		try {
+			this.repeatRule = Selects.repeatRule(0);
+		} catch (DBObjectNotFoundException e) {
+			// TODO default RepeatRule 0
+			e.printStackTrace();
+		}
+		*/
+	}
+
+	/**
+	 * Instantiates a new to do with all parameters and foreign keys.
+	 *
+	 * @param toDoId the to do id
+	 * @param date the date
+	 * @param active the active
+	 * @param repeatRule the repeat rule
+	 */
+	public ToDo(int toDoId, Date date, boolean active, RepeatRule repeatRule) {
+		this.toDoId = toDoId;
+		this.date = date;
+		this.active = active;
+		this.repeatRule = repeatRule;
+	}
+
+	/**
+	 * Gets the ToDo id.
+	 *
+	 * @return the ToDo id
+	 */
+	public int getToDoId() {
+		return toDoId;
+	}
+
+	/**
+	 * Sets the ToDo id.
+	 *
+	 * @param toDoId the new ToDo id
+	 */
+	public void setToDoId(int toDoId) {
+		this.toDoId = toDoId;
+	}
+
+	/**
+	 * Gets the date when this ToDo will be executed.
+	 *
+	 * @return the date when this ToDo will be executed
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * Sets the date when this ToDo will be executed.
+	 *
+	 * @param date the new date when this ToDo will be executed
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	/**
+	 * Checks if this ToDo is active.
+	 *
+	 * @return true, if this ToDo is active
+	 */
+	public boolean isActive() {
+		return active;
+	}
+
+	/**
+	 * activate or deactivate this ToDo.
+	 *
+	 * @param active True for activate and false for deactivate
+	 */
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	/**
+	 * Gets the RepeatRule this ToDo is belonging to.
+	 *
+	 * @return the RepeatRule this ToDo is belonging to
+	 */
+	public RepeatRule getRepeatRule() {
+		return repeatRule;
+	}
+
+	/**
+	 * Sets the RepeatRule this ToDo is belonging to.
+	 *
+	 * @param repeatRule the new RepeatRule this ToDo is belonging to
+	 */
+	public void setRepeatRule(RepeatRule repeatRule) {
+		this.repeatRule = repeatRule;
+	}
+
+	/**
+	 *  
+	 * To compare two ToDos.
+	 *
+	 * @param obj the ToDo object to compare
+	 * @return true if both ToDos are equal
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ToDo other = (ToDo) obj;
+		if (active != other.active)
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (repeatRule == null) {
+			if (other.repeatRule != null)
+				return false;
+		} else if (!repeatRule.equals(other.repeatRule))
+			return false;
+		if (toDoId != other.toDoId)
+			return false;
+		return true;
+	}
+
+}
